32af91e45129e52ed40275e98d8e3148cb5f4b0d
WhizSid <whizsid@aol.com>
2025-06-13 10:18:59 +0530

    Removed the send bound for WASM32

--- src/program.rs
+++ src/program.rs
@@ -16,9 +16,9 @@ pub trait Program: Sized {
     type State;
 
     /// The message of the program.
-    #[cfg(not(target_family = "wasm32"))]
+    #[cfg(not(target_arch = "wasm32"))]
     type Message: Send + std::fmt::Debug + 'static;
-    #[cfg(target_family = "wasm32")]
+    #[cfg(target_arch = "wasm32")]
     type Message: std::fmt::Debug + 'static;
 
     /// The theme of the program.
